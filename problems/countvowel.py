6c4092db5008071e416d2cc825bd83ddee1898dd
SubhanAli-creator <subhanmajeed469@gmail.com>
2025-07-16 12:35:45 -0700

    Count Vowels in Text

---
+++ problems/countvowel.py
@@ -0,0 +1,9 @@
+def count_vowel(text):
+    total = 0
+    vowels = ['a','e','i','o','u']
+    for x in text.strip():
+        if x.lower() in vowels:
+            total+=1
+    return total
+text = str(input("Enter Text: "))
+print(count_vowel(text))
