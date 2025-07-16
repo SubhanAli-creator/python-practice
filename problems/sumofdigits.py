9ba6da412bbcfa0ba8d2e50f73a1c19adb7b98d6
SubhanAli-creator <subhanmajeed469@gmail.com>
2025-07-16 12:25:27 -0700

    sum of digits

---
+++ problems/sumofdigits.py
@@ -0,0 +1,11 @@
+def sum_of_digit(digit):
+    digit = abs(digit)
+    total = 0
+    while digit > 0:
+        single = digit%10
+        digit//=10
+        sum+=single
+    return total
+number = int(input("Enter Your Number: "))
+print(sum_of_digit(number))
+        
